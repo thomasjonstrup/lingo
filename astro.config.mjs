b05869c7f37eb780ab9dece7d348070b78fc670d
Thomas Jonstrup <thomas_jonstrup@hotmail.com>
2024-03-17 23:07:12 +0100

    Vercel update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+// Import /serverless for a Serverless SSR site
+import vercelServerless from '@astrojs/vercel/serverless';
 
 import db from '@astrojs/db';
 
 // https://astro.build/config
 export default defineConfig({
+	output: 'server',
+	adapter: vercelServerless(),
 	integrations: [tailwind(), db()],
 });
--- db/seed.ts
+++ db/seed.ts
@@ -3,35 +3,49 @@ import { db, Comment, Author, Course } from 'astro:db';
 // https://astro.build/db/seed
 export default async function seed() {
 	await db.insert(Author).values([
-		{ id: 1, name: 'Testing'},
-		{ id: 2, name: 'Tester'}
+		{ id: 1, name: 'Testing' },
+		{ id: 2, name: 'Tester' },
 	]);
 
 	await db.insert(Comment).values([
-		{ authorId: 1, body: 'dope you like Astro DB!',flagged: false, likes: 0, metadata: '', published: new Date()},
-		{ authorId: 2, body: 'Enjoy',flagged: false, likes: 0, metadata: '', published: new Date()},
+		{
+			authorId: 1,
+			body: 'dope you like Astro DB!',
+			flagged: false,
+			likes: 0,
+			metadata: '',
+			published: new Date(),
+		},
+		{
+			authorId: 2,
+			body: 'Enjoy',
+			flagged: false,
+			likes: 0,
+			metadata: '',
+			published: new Date(),
+		},
 	]);
 
 	await db.insert(Course).values([
 		{
 			id: 1,
 			title: 'Spanish',
-			imageSrc: '/es.svg'
+			imageSrc: '/es.svg',
 		},
-		      {
-        id: 2,
-        title: "Italian",
-        imageSrc: "/it.svg",
-      },
-      {
-        id: 3,
-        title: "French",
-        imageSrc: "/fr.svg",
-      },
-      {
-        id: 4,
-        title: "Croatian",
-        imageSrc: "/hr.svg",
-      },
-	])
+		{
+			id: 2,
+			title: 'Italian',
+			imageSrc: '/it.svg',
+		},
+		{
+			id: 3,
+			title: 'French',
+			imageSrc: '/fr.svg',
+		},
+		{
+			id: 4,
+			title: 'Croatian',
+			imageSrc: '/hr.svg',
+		},
+	]);
 }
